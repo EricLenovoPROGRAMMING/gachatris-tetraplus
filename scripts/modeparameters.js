cabf79e4bafd230cdda446c491cfaa8cffa805ec
EricLenovo, Programming <98129619+EricLenovoPROGRAMMING@users.noreply.github.com>
2022-05-25 13:15:42 +0800

    Update May 5, 2022, to be used by future updates

---
+++ scripts/modeparameters.js
@@ -0,0 +1,24 @@
+var modeParameters = {
+  selected: {
+  linerun: {
+    LINE: 40,
+    TYPE: 0
+  }
+  },
+  parameters: {
+    linerun: {
+      LINE: range(20, 1020,20),
+      TYPE: range(0,4)
+    }
+  },
+  texts: {
+    linerun: {
+      LINE: range(20, 1020,20),
+      TYPE: [`Normal (7 bags of 7 Gachaminoes`,`Heaven (I only)`,`L_ne Run (Missing I-piece)`]
+    }
+  }
+}
+
+function modeParamSwitch(){
+  
+}
