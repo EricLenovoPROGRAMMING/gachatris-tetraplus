fedc783b6ab7e21dd7ce1a696a82bfea29a8c900
EricLenovo, Programming <98129619+EricLenovoPROGRAMMING@users.noreply.github.com>
2022-05-25 11:48:11 +0800

    Update May 25, 2022

--- initializer.js
+++ initializer.js
@@ -1,105 +1,143 @@
-//Shorten document.getElementById
+/**EricLenovo 2022
+ * Gachatris Tetraplus Initializer
+ */
+
+/*eval = function() {
+  return console.warn('JS Console is not usable in this site.')
+}/**/
+
 function docId(id) {
- return document.getElementById(id)
-}
-//Shorten document.getElementsbyClassName
-function $CN(id,n) {
- if (n !== void 0)
- return document.getElementsByClassName(id)[n]
- else
- return document.getElementsByClassName(id)
-}
-//Shorten document.getElementsByTagName
-function $tag(id,n){
- if (n !== void 0)
-  return document.getElementsByTagName(id)[n]
- else
-  return document.getElementsByTagName(id)
-}
-//Shorten document.createElement
-async function $create(tag, func){
- let a = document.createElement(tag)
- await func(a)
+  return document.getElementById(id)
 }
 
-//Create array of ranges
+function $CN(id, n) {
+  if (n !== void 0)
+    return document.getElementsByClassName(id)[n]
+  else
+    return document.getElementsByClassName(id)
+}
+
+function $tag(id, n) {
+  if (n !== void 0)
+    return document.getElementsByTagName(id)[n]
+  else
+    return document.getElementsByTagName(id)
+}
+
+async function $create(tag, func) {
+  let a = document.createElement(tag)
+  await func(a)
+}
+
+
 function range(start, end, inc) {
- inc = inc || 1
- var array = []
- for (var i = start; i < end; i += inc) {
-  array.push(i)
- }
- return array
+  inc = inc || 1
+  var array = []
+  for (var i = start; i < end; i += inc) {
+    array.push(i)
+  }
+  return array
 }
-//shorten docId().innerHTML
-function $iH(id,innerHTML){
- docId(id).innerHTML=innerHTML
+
+function $iH(id, innerHTML) {
+  docId(id).innerHTML = innerHTML
 }
-//Number Modulator for tetrimino clock arithmetics
+
 Number.prototype.mod = function(n) {
- return ((this % n) + n) % n
+  return ((this % n) + n) % n
 };
 
-//initialize Gachatris
-(function(){
- let array=['jQuery','howler-lib','gachatris-data','piece','field','queue_prev','pieceHold','soundplayer','main', 'menus']
- var i=0
- function loadScript(){
-  $create('script', function(a){
-   a.src=`scripts/${array[i]}.js`
-   a.id=`script-${array[i]}`
-   a.type='text/javascript'
-   document.body.appendChild(a)
-   a.onload = ()=>{
-    if(i<array.length -1){
-    i++
-    loadScript()
 
-    } else {
-     initializeGTris()
-    }
+(function() {
+  let array = ['jQuery', 'howler-lib', 'gachatris-data', 'piece', 'field', 'queue_prev', 'pieceHold', 'soundplayer','musicplayer', 'language', 'main', 'character_details','menus', 'modeparameters']
+  var i = 0
+  var sfx
+  function loadScript() {
+    $create('script', function(a) {
+      a.src = `scripts/${array[i]}.js`
+      a.id = `script-${array[i]}`
+      a.type = 'text/javascript'
+      document.body.appendChild(a)
+      a.onload = () => {
+        if (i < array.length - 1) {
+          i++
+          loadScript()
+        } else {
+          sound()
+        }
+      }
+    })
   }
- })
- }
- loadScript()
- 
- function initializeGTris(){
-  $('#gtrisSplashText').animate({opacity: 0}, 0, "linear",function(){this.innerHTML='EricLenovo presents...'})
-  $('#gtrisSplashText').animate({opacity: 1}, 400, "linear")
-  $('#gtrisSplashText').animate({opacity: 1}, 1000, "linear")
-  $('#gtrisSplashText').animate({opacity: 0}, 400, "linear",function(){this.innerHTML='Developed on May 5, 2022'})
-  $('#gtrisSplashText').animate({opacity: 1}, 400, "linear")
-  $('#gtrisSplashText').animate({opacity: 1}, 1500, "linear")
-  $('#gtrisSplashText').animate({opacity: 0}, 400, "linear",function(){
-   this.innerHTML=`Gachatris Tetraplus, v${gtris_version}`
-   $('#splashLogo').animate({opacity: 0}, 0, "linear")
-   $('#splashLogo').animate({opacity: 1}, 400, "linear")
-   $('#splashLogo').animate({opacity: 1}, 1000, "linear")
-   $('#splashLogo').animate({opacity: 0}, 250, "linear")
-  })
-  $('#gtrisSplashText').animate({opacity: 1}, 400, "linear")
-  $('#gtrisSplashText').animate({opacity: 1}, 1000, "linear")
-  $('#gtrisSplashText').animate({opacity: 0}, 250, "linear", function(){
-   $('#splash').css('z-index', "-36")
-   activeMenu(true, "100%", true)
-   switchMenu(0, true, 'Gachatris Tetraplus Alpha', true)
-   addEventListener('keydown', keyUpDown, false);
-   addEventListener('keyup', keyUpDown, false);
-  })
+  function startup(){
+  sfx.play()
+}
+  function sound() {
+    addEventListener("click", startup, false)
+    addEventListener("keydown", startup, false)
+    sfx = new Howl({ src: "assets/menu/uise/gtrisstartup.ogg", preload: true, format: "ogg"})
+    sfx.volume(selectedSettings.Volume.Music / 100)
+    sfx.once('load', function() {
+      setTimeout(()=>{
+      sfx.play()
+      },100)
+    })
+    sfx.once('play',function(){
+      initializeGTris()
+      removeEventListener("click", startup, false)
+      removeEventListener("keydown", startup, false)
+    })
+    setTimeout(()=>{
+      sfx.load()
+      },100)
+    musicPlayer.loadMfx('menu')
+  }
+  loadScript()
+  function skipWithKey() {
+    var arr = [$('#gtrisSplashText'), $('#splashLogo')]
+    arr.forEach((e) => {
+      e.stop(true, true)
+      e.css({ 'opacity': 0 })
+    })
+    skip()
+    sfx.fade(selectedSettings.Volume.Music / 100, 0, 500)
+    sfx.on('volume', function() { this.unload() })
+  }
 
- //$('#splash').css('display','none')
- }
- 
+  function skip() {
+    $('#splash').css('z-index', "-36")
+    activeMenu(true, "100%", true)
+    switchMenu(0, true, 'Gachatris Tetraplus Alpha', true)
+    removeEventListener('keydown', skipWithKey)
+    removeEventListener('click', skipWithKey)
+    musicPlayer.playMfx('menu')
+    addEventListener('keydown', keyUpDown, false)
+    addEventListener('keyup', keyUpDown, false)
+  }
+
+  function initializeGTris() {
+    setTimeout(function() {
+      sfx.once('end', function() { sfx.unload() })
+      addEventListener('keydown', skipWithKey, false)
+      addEventListener('click', skipWithKey, false)
+    }, 300)
+    $('#gtrisSplashText').animate({ opacity: 0 }, 0, "linear", function() { this.innerHTML = gtris_transText('splash1') })
+    $('#gtrisSplashText').animate({ opacity: 1 }, 400, "linear")
+    $('#gtrisSplashText').animate({ opacity: 1 }, 2500, "linear")
+    $('#gtrisSplashText').animate({ opacity: 0 }, 400, "linear", function() { this.innerHTML = gtris_transText('splash2') })
+    $('#gtrisSplashText').animate({ opacity: 1 }, 400, "linear")
+    $('#gtrisSplashText').animate({ opacity: 1 }, 2500, "linear")
+    $('#gtrisSplashText').animate({ opacity: 0 }, 400, "linear", function() {
+      this.innerHTML = gtris_transText('splash3', gtris_version)
+      $('#splashLogo').animate({ opacity: 0 }, 0, "linear")
+      $('#splashLogo').animate({ opacity: 1 }, 400, "linear")
+      $('#splashLogo').animate({ opacity: 1 }, 1000, "linear")
+      $('#splashLogo').animate({ opacity: 0 }, 250, "linear")
+    })
+    $('#gtrisSplashText').animate({ opacity: 1 }, 400, "linear")
+    $('#gtrisSplashText').animate({ opacity: 1 }, 1000, "linear")
+    $('#gtrisSplashText').animate({ opacity: 0 }, 250, "linear", function() {
+      skip()
+    })
+  }
 })()
-
-//Globally handle errors
-window.onerror = (event,source,lineno,colno,error) =>{
- if(error instanceof SyntaxError)
- document.body.innerHTML = (`FATAL ERROR!!! At ${source}, ${lineno}:${colno}, there is ${['a','e','i','o','u'].indexOf(event.toLowerCase().replace('uncaught syntaxerror: ', '').charAt(0)) !== -1?'an':'a'} ${event.replace('Uncaught SyntaxError: ','').toLowerCase()}. Please contact the Gachatris developer and he will fix a discovered bug to recover the game.`)
- else{
- alert(`At ${source}, ${lineno}:${colno}, there is ${[`a`,`e`,`i`,`o`,`u`].indexOf(event.toLowerCase().charAt(0)) !== -1?'an':'a'} ${event}. If you see this error mesage, contact the Gachatris developer.`)
-}
-}
-
-
 
