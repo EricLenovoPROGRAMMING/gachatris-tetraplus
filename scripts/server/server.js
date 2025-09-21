196b086f96788c4576075e8e48e935caa7f7b61d
EricLenovo, Programming <98129619+EricLenovoPROGRAMMING@users.noreply.github.com>
2025-09-21 20:45:34 +0000

    Rewrite for 1.1.5474 Online Test Alpha, Release O_01

--- scripts/server/server.js
+++ scripts/server/server.js
@@ -106,7 +106,7 @@ class BinaryReader {
 
 
 const server = new class {
-	#url = "wss://curly-paraleet-9gqqv7xgx442px54.github.dev:2400";
+	#url = "wss://curly-parakeet-9gqqv7xgx442px54.github.dev:2400";
 	#socket = {};
 	userID = null;
 	roomID = null;
